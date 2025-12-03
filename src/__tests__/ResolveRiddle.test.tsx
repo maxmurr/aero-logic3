cfa6a647997b6d7e52ddec23fe580a8c0ccaa492
maxmurr <maxmurr.m@gmail.com>
2025-12-03 16:14:24 +0700

    test: Enhance riddle resolution tests with success and failure scenarios

--- src/__tests__/ResolveRiddle.test.tsx
+++ src/__tests__/ResolveRiddle.test.tsx
@@ -1,15 +1,19 @@
-import { App } from "../App";
+import { App } from '../App';
 import { ContextProvider } from '../common/context';
 import { provideRiddleAnswer } from '../domain/riddle/riddle-answer-provider';
 
 describe('Resolve riddle', () => {
     it('resolve riddle successfully', () => {
         cy.intercept('GET', '**/riddles/RIDDLE_ID', {
-            body: { id: 'RIDDLE_ID', contents: 'My riddle contents', answers: [
-                { id: '1', text: 'Answer 1' },
-                { id: '2', text: 'Answer 2' },
-                { id: '3', text: 'Answer 3' },
-            ] },
+            body: {
+                id: 'RIDDLE_ID',
+                contents: 'My riddle contents',
+                answers: [
+                    { id: '1', text: 'Answer 1' },
+                    { id: '2', text: 'Answer 2' },
+                    { id: '3', text: 'Answer 3' },
+                ],
+            },
         });
 
         const fake = () => Promise.resolve({ id: '1', text: 'Answer 1' });
@@ -28,6 +32,40 @@ describe('Resolve riddle', () => {
 
         cy.contains('Answer 1').click();
 
-        cy.contains('Great job! You\'re right 🙏').should('be.visible');
-    })
-})
+        cy.contains("Great job! You're right 🙏").should('be.visible');
+    });
+
+    it('resolve riddle unsuccessfully', () => {
+        cy.intercept('GET', '**/riddles/RIDDLE_ID', {
+            body: {
+                id: 'RIDDLE_ID',
+                contents: 'My riddle contents',
+                answers: [
+                    { id: '1', text: 'Answer 1' },
+                    { id: '2', text: 'Answer 2' },
+                    { id: '3', text: 'Answer 3' },
+                ],
+            },
+        });
+
+        const fake = () => Promise.resolve({ id: '1', text: 'Answer 1' });
+
+        cy.mount(
+            <ContextProvider providers={[provideRiddleAnswer(fake)]}>
+                <App />
+            </ContextProvider>,
+            '/riddle/RIDDLE_ID',
+        );
+
+        cy.contains('My riddle contents').should('be.visible');
+        cy.contains('Answer 1').should('be.visible');
+        cy.contains('Answer 2').should('be.visible');
+        cy.contains('Answer 3').should('be.visible');
+
+        cy.contains('Answer 2').click();
+
+        cy.contains("This time your answer is wrong.").should('be.visible');
+    });
+
+
+});
